28f3e63f34632126673c6be5897db214340bf04b
Farid Vakili <fvakili@cpp.edu>
2022-10-20 12:46:02 -0700

    First commit, Outline of code structure and ideas of how to implement

---
+++ restaurant-bill-main.c
@@ -0,0 +1,31 @@
+/* git reminders
+git remote add origin https://github.com/justFreds/Restaurant_Bill_Quiz.git
+git branch -M main
+git push -u origin main 
+*/
+
+//STRUCT OF MENU ITEMS WITH THERE COSTS
+
+//MAIN FUNCTION
+//ACCEPTS TWO ARGUMENTS [TAX][TIP]-FROM USER
+
+
+//RANDOMIZE THE MENU ITEM SELECTED
+
+//PASS THE TAX AND TIP INTO FUNCTION [RETURNS TOTAL]
+
+//DISPLAY: MENU ITEM CHOSEN
+//              COST
+//              TIP
+//              TAX
+//              TIP
+//              TOTAL
+
+
+
+
+/*
+    FUNCTION TO CALCULATE TOTAL [cost, tip, tax]
+    This is where we'll be calculating the cost
+    Total
+*/
