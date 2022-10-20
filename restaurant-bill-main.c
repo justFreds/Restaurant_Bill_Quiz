e7f43c25b896c978605c928e7b166b9eb9575641
Farid Vakili <fvakili@cpp.edu>
2022-10-20 13:43:03 -0700

    Created a random number to select from the MenuItemEntries[0-3] 4 items.

--- restaurant-bill-main.c
+++ restaurant-bill-main.c
@@ -3,23 +3,31 @@ git remote add origin https://github.com/justFreds/Restaurant_Bill_Quiz.git
 git branch -M main
 git push -u origin main 
 */
-#include <stdio.h>  
-#include <stddef.h> 
+#include "restaurant-items.h"  
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-//STRUCT OF MENU ITEMS WITH THERE COSTS
-typedef struct {
-    char *menuItem; //4bytes
-    double initialCost; //8bytes
-    //worst case scenario is 8bytes so previous become 8
-    //total of 16 bytes 
-} MenuItem, *PtrToMenuItem; 
-typedef const MenuItem *PtrToMenuItem; 
+
 
 //MAIN FUNCTION
 //ACCEPTS TWO ARGUMENTS [TAX][TIP]-FROM USER
+int main(int argc, char *argv[]) {  //argc == argument count && argv == argument value (array size)
+
+    extern MenuItem Item_Table[];  
+    extern const int MenuItemEntries;
+
+    //create a random number to select item from MenuItem Array send into function
+    time_t t;
+    srand((unsigned)time(&t));
+    int randMenuItem = rand() % MenuItemEntries;
+/*     for(int i = 0; i <= 20; i++) {
+        printf("Random#: %d  -Output: %d\n", i, randMenuItem);
+    } */
+
+    
+    return EXIT_SUCCESS;
+}
 
 
 //RANDOMIZE THE MENU ITEM SELECTED
