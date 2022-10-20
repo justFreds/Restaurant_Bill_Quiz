8f3db6ad2f02dc2083e17a6ce8688df66f8c3935
Farid Vakili <fvakili@cpp.edu>
2022-10-20 12:52:48 -0700

    Defined a struct of menu items but thinking of making a different file for them and another with a table of the items.

--- restaurant-bill-main.c
+++ restaurant-bill-main.c
@@ -3,8 +3,20 @@ git remote add origin https://github.com/justFreds/Restaurant_Bill_Quiz.git
 git branch -M main
 git push -u origin main 
 */
+#include <stdio.h>  
+#include <stddef.h> 
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 //STRUCT OF MENU ITEMS WITH THERE COSTS
+typedef struct {
+    char *menuItem; //4bytes
+    double initialCost; //8bytes
+    //worst case scenario is 8bytes so previous become 8
+    //total of 16 bytes 
+} MenuItem, *PtrToMenuItem; 
+typedef const MenuItem *PtrToMenuItem; 
 
 //MAIN FUNCTION
 //ACCEPTS TWO ARGUMENTS [TAX][TIP]-FROM USER
